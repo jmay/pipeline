5ff3ba2ab65fc522f9fcecbfc5fc9428cd8c3fcb
Jason May <jmay@pobox.com>
2008-06-25 12:52:48 -0700

    BugzID: 236 capture curl errors, write out processing results as YAML, works with source_worker.rb

--- bin/download.pl
+++ bin/download.pl
@@ -3,7 +3,10 @@
 use strict;
 use Getopt::Long; # for GetOptions()
 use Data::UUID;
-use File::Path 'mkpath';
+use File::Path; # for mkpath, rmtree
+use File::Temp;   # for tempdir()
+use File::Copy;
+use YAML::Syck;
 
 =head1 NAME
 
@@ -49,16 +52,47 @@ die $USAGE, "\n" unless defined($root) && defined($uri);
 
 ##################################################
 
-my $uuid = lc(Data::UUID->new->create_str);
-my ($one, $two) = $uuid =~ /^(..)(..)/;
-my $fullpath = "$root/$one/$two/$uuid";
+my $dir = File::Temp::tempdir("numbrary.XXXX", DIR => $ENV{TMPDIR});
+# print STDERR "Output to $dir\n";
+# chdir $dir;
 
-mkpath("$root/$one/$two"); # no error checking, OK if path exists already
+# TODO: deal with redirects, needs to pass back the ultimate target URI
+# TODO: consider doing automatic retries with curl --retry
+# TODO: consider more info in write-out
 
-my $cmd = "curl --silent '$uri' > $fullpath";
-print STDERR "$cmd\n";
-(system($cmd) == 0) or die $!;
+my $output = "output";
 
-print $uuid;
+my $cmd = "curl --fail --silent '$uri' --write-out '%{http_code} %{content_type}' --output '$dir/$output'";
+my $results = `$cmd`;
+my $curl_error = $? >> 8;
+
+my ($http_status, $content_type) = $results =~ /^(\d+) (.*)/;
+
+my $stats = {};
+
+if ($curl_error == 0) {
+  $stats->{':http_status'} = $http_status,
+  $stats->{':content_type'} = $content_type,
+
+  my $uuid = lc(Data::UUID->new->create_str);
+  my ($one, $two) = $uuid =~ /^(..)(..)/;
+  my $fullpath = "$root/$one/$two/$uuid";
+
+  mkpath("$root/$one/$two"); # no error checking, OK if path exists already
+  move("$dir/$output", $fullpath);
+
+  $stats->{':storage_id'} = $uuid;
+} else {
+  $stats->{':curl_error'} = $curl_error;
+}
+
+# my $cmd = "curl --silent '$uri' > $fullpath";
+# print STDERR "$cmd\n";
+# (system($cmd) == 0) or die $!;
+
+print Dump($stats);
+# print $uuid;
+
+rmtree($dir);
 
 exit;
