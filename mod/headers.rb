778c65d8fd5a18241b7320bcc265ee8dc472bf60
Jason May <jmay@pobox.com>
2008-07-22 12:48:06 -0700

    needs more resilience in header parsing - source could have multiple rows of headers with different # of fields

--- mod/headers.rb
+++ mod/headers.rb
@@ -3,6 +3,7 @@
 require "fastercsv"
 require "getoptlong"
 require "yaml"
+require "pp"
 
 opts = GetoptLong.new(
   [ '--rows', GetoptLong::REQUIRED_ARGUMENT ]
@@ -26,12 +27,15 @@ nrows = 0
 
 header_lines = []
 header_rows.times { header_lines << $stdin.readline.chomp }
+headers = header_lines.map {|line| line.split(/\t/)}
+max_header_cols = headers.map{|row| row.size}.max
+headers.each {|row| row[max_header_cols-1] ||= nil} # pad all headers to same width so transpose will work
 
-headers = header_lines.map {|line| line.split(/\t/)}.transpose.map {|fields| fields.join(' ')}
+headers = headers.transpose.map {|fields| fields.join(' ').strip}
 
+# run the rest of the input straight through to the next pipeline stage
 $stdin.each_line do |line|
   puts line
-
   nrows += 1
 end
 
