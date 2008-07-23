d2329e6cd09f611f9437d8a015dd24d3b40bd6fc
Jason May <jmay@pobox.com>
2008-07-23 11:59:07 -0700

    for whitespace sources, truncate leading blanks (always, might make it configurable later)

--- mod/whitespace_separated.rb
+++ mod/whitespace_separated.rb
@@ -1,16 +1,43 @@
 #!/usr/bin/env ruby
 
+require "getoptlong"
 require "yaml"
 
-nrows = 0
+# opts = GetoptLong.new(
+#   [ '--ignore-leading', GetoptLong::REQUIRED_ARGUMENT ]
+# )
+# 
+# ignore_leading = 0
+# begin
+#   opts.each do |opt, arg|
+#     case opt
+#     when '--ignore-leading'
+#       ignore_leading = arg.to_i
+#     end
+#   end
+# rescue
+#   puts "BAD OPTIONS"
+#   exit 1
+# end
+
+nrows = rejected_rows = 0
 
 $stdin.each_line do |line|
   fields = line.split(/\s+/)
-  puts fields.join("\t")
-  nrows += 1
+  fields.shift while fields.first == '' # always truncate leading blanks
+  # if ignore_leading
+  #   fields.shift while fields.first == ''
+  # end
+  if fields.any?
+    puts fields.join("\t")
+    nrows += 1
+  else
+    rejected_rows += 1
+  end
 end
 
 stats = {
-  :nrows => nrows
+  :nrows => nrows,
+  :rejected_rows => rejected_rows,
 }
 $stderr.puts stats.to_yaml
