046bfee04746fe16f059725b0a0cea173076cef4
Jason May <jmay@pobox.com>
2008-07-24 12:22:37 -0700

    html module knows about tables (converts them to TSV), and can look for 'nth' match to an xpath expression

--- mod/html.rb
+++ mod/html.rb
@@ -6,17 +6,26 @@ require "getoptlong"
 require "hpricot"
 require "htmlentities"  # for converting HTML entities like &nbsp;
 require "yaml"
+require "pp"
 
 opts = GetoptLong.new(
-  [ '--xpath', GetoptLong::REQUIRED_ARGUMENT ]
+  [ '--xpath', GetoptLong::REQUIRED_ARGUMENT ],
+  [ '--nth', GetoptLong::REQUIRED_ARGUMENT ],
+  [ '--table', GetoptLong::REQUIRED_ARGUMENT ]
 )
 
-xpath = nil
+xpath = nth = nil
+parsetable = false
+
 begin
   opts.each do |opt, arg|
     case opt
     when '--xpath'
       xpath = arg
+    when '--nth'
+      nth = arg.to_i
+    when '--table'
+      parsetable = true
     end
   end
 rescue Exception => e
@@ -24,12 +33,39 @@ rescue Exception => e
   exit 1
 end
 
-coder = HTMLEntities.new
+Coder = HTMLEntities.new
 doc = Hpricot(STDIN.readlines.join)
 
-puts coder.decode(doc.search(xpath).inner_html)
+def mydecode(cell)
+  Coder.decode(cell.inner_text.gsub(/&nbsp;/, ' ').gsub(/[[:space:]]+/, ' ').gsub(/\?/, '')).strip
+end
+
+node = doc.search(xpath)
+if nth
+  node = node[nth]
+end
+
+# (table/"/tr").each do |row|
+#   rowcells = (html_table_row_cells(row)).map {|cell| cell.inner_text.strip.gsub(/[$,]/, '').gsub(/\s+/, ' ')}
+#   if rowcells.find {|cell| cell =~ /\S/}
+#     # there's at least one cell that has a non-blank in it
+#     lines << rowcells
+#   end
+
+if parsetable
+  node.search("tr").each do |tr|
+    row = tr.search("td,th").map {|cell| mydecode(cell)}
+    if row.find_all{|cell| cell =~ /[^[:space:]]/}.any?
+      puts row.join("\t")
+    end
+  end
+else
+  puts Coder.decode(node.inner_html)
+end
 
 
 stats = {
 }
 $stderr.puts stats.to_yaml
+
+
