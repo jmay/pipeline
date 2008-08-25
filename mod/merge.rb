28bf5adecd4c329873962d12a007d9cb985ae2c6
Jason May <jmay@pobox.com>
2008-08-25 13:54:15 -0700

    changed default behavior of merge.rb to ignore input rows until the earlier chron value for which both sources have values (get old behavior, includes all the rows, using '--retain 1' parameter)

--- mod/merge.rb
+++ mod/merge.rb
@@ -6,20 +6,24 @@
 # 
 # =head1 DESCRIPTION
 # 
-# Read secondary source as TSV
+# Take two sources and merge them.  Merge means to concatenate the data columns in the sources
+# based on matches between the grouping columns.  The output is sorted based on the grouping columns.
+# By default early (low-sorting by grouping columns) rows will be dropped until the first record
+# that contains values from both sources.  If '--retain 1' is specified then even those early records
+# will be included.
 # 
 # Read primary source from stdin, merge with secondary source, write merged records to stdout
 # 
-# Secondary fields will be injected into the output immediately following the merge column
+# Data columns from the secondary source are injected into the output following the data columns from
+# the primary source.
 # 
 # =head1 CONTRACT
 # 
-# Standard expectations (TSV, no blank lines, stripped, UTF-8) for both source files
+# Standard expectations (TSV, no blank lines, stripped, squeezed, UTF-8) for both source files
 # 
-# Expects that secondary source rows will be unique based on the key column;
-# this expectation does not apply to the primary source
+# Expects that both the primary and secondary sources rows will be unique based on the grouping columns.
 # 
-# Output will be normalized TSV also
+# Output will be normalized TSV also.
 # 
 # =head1 OPTIONS
 # 
@@ -27,6 +31,7 @@
 #   --group1 colnum [REQUIRED] list of columns in stdin to match on (starting from zero)
 #   --group2 colnum [REQUIRED] list of columns in secondary source to match with (starting from zero)
 #   --pick2 colnum [REQUIRED] list of columns in secondary source to append to original columns
+#   --retain 0|1 (default 0) whether to keep early data with just one of the sources
 # 
 # =head1 TODO
 # 
@@ -44,13 +49,15 @@ opts = GetoptLong.new(
   [ '--input', GetoptLong::REQUIRED_ARGUMENT ],
   [ '--group1', GetoptLong::REQUIRED_ARGUMENT ],
   [ '--group2', GetoptLong::REQUIRED_ARGUMENT ],
-  [ '--pick2', GetoptLong::REQUIRED_ARGUMENT ]
+  [ '--pick2', GetoptLong::REQUIRED_ARGUMENT ],
+  [ '--retain', GetoptLong::REQUIRED_ARGUMENT ]
 )
 
 source2_file = nil
 source1_keycols = []
 source2_keycols = []
 source2_datacols = []
+retain = false
 
 begin
   opts.each do |opt, arg|
@@ -63,6 +70,8 @@ begin
       source2_keycols = arg.split(/\s*,\s*/).map(&:to_i).sort
     when '--pick2'
       source2_datacols = arg.split(/\s*,\s*/).map(&:to_i).sort
+    when '--retain'
+      retain = true if arg.to_i > 0
     end
   end
 rescue
@@ -74,6 +83,7 @@ source1_data = {}
 source2_data = {}
 source1_ncolumns = 0
 
+# load source 2
 nrows2 = 0
 File.open(source2_file).each_line do |line|
   row = line.chomp.split(/\t/)
@@ -83,6 +93,7 @@ File.open(source2_file).each_line do |line|
   nrows2 += 1
 end
 
+# load source 1
 nrows1 = 0
 $stdin.each_line do |line|
   row = line.chomp.split(/\t/)
@@ -101,19 +112,24 @@ source2_data.keys.each do |key|
 end
 
 
+# output rows in chron order
+foundboth = false # haven't found records that appear in both sources yet
 nrows = 0
 merge_keys.keys.sort.each do |key|
   inrow = source1_data[key]
   if inrow.nil?
+    next unless foundboth || retain # don't output anything until first record with both sources
     # data only in source2, use blanks in the source1 columns
     outrow = key.dup
     outrow.concat(Array.new(source1_ncolumns - source1_keycols.size))
     outrow.concat(source2_data[key])
   elsif source2_data[key]
     # data in both source1 and source2
+    foundboth = true
     outrow = inrow.dup
     outrow.concat(source2_data[key])
   else
+    next unless foundboth || retain
     # data only in source1
     outrow = inrow.dup
     outrow.concat(Array.new(source2_datacols.size))
