addab66f2526a932eec124db1b77d95b0e6c68c2
Jason May <jmay@pobox.com>
2008-10-15 11:59:49 -0700

    xls.py now replicates values inside excel merged-cell areas

--- mod/xls.py
+++ mod/xls.py
@@ -9,24 +9,50 @@ import datetime
 # slurp entire input into string
 input = sys.stdin.read()
 
-# book = xlrd.open_workbook(filename = "/dev/stdin")
-book = xlrd.open_workbook(file_contents = input)
+# with default formatting_info=False it doesn't pull the merged_cells info
+book = xlrd.open_workbook(file_contents = input, formatting_info = True)
 sheet = book.sheet_by_index(0)
+
+# gather all the merge-cell info
+merges = {}
+for crange in sheet.merged_cells:
+  rlo, rhi, clo, chi = crange
+  for rowx in xrange(rlo, rhi):
+    for colx in xrange(clo, chi):
+      key = "%(rowx)d,%(colx)d" % vars()
+      merges[key] = "%(rlo)d,%(clo)d" % vars()
 
 writer = csv.writer(sys.stdout, delimiter = "\t")
 
 for rownum in range(sheet.nrows)[0:]:
   row = []
 
-  for cell in sheet.row(rownum):
+  for colnum, cell in enumerate(sheet.row(rownum)):
+
     if cell.ctype == 3:
       # date
       dtuple = xlrd.xldate_as_tuple(cell.value, book.datemode)
       row.append(datetime.date(dtuple[0], dtuple[1], dtuple[2]).strftime("%Y-%m-%d"))
     else:
       # numbers and strings
-      row.append(cell.value)
+      mergekey = "%(rownum)d,%(colnum)d" % vars()
 
+      if cell.value == "":
+        # cell is empty, look in the merge store to see if we need to replicate a value
+        if mergekey in merges:
+          row.append(merges[merges[mergekey]])
+        else:
+          row.append("")
+      else:
+        row.append(cell.value)
+
+        # if this is the top-left cell in a merge area, then stash the value for replication later
+        if mergekey in merges:
+          if merges[mergekey] == mergekey:
+            merges[mergekey] = cell.value
+
+  while row[-1] == "":
+    row.pop()
   writer.writerow(row)
 
 stats = { ':nrows': sheet.nrows }
